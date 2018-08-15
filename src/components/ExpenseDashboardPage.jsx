60f98a9c8060a47d805ce2d0272a39b873d706a0
NearHuscarl <near.huscarl@gmail.com>
2018-08-15 18:41:18 +0700

    oops

--- src/components/ExpenseDashboardPage.jsx
+++ src/components/ExpenseDashboardPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
 	<div>
+		<ExpensesSummary />
 		<ExpenseListFilters />
 		<ExpenseList />
 	</div>
