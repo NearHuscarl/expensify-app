6327c17e8467a78f51a3f4b09ea5a8dde93efdbe
NearHuscarl <near.huscarl@gmail.com>
2018-08-20 23:52:59 +0700

    add hidden expense count in summary

--- src/components/ExpensesSummary.jsx
+++ src/components/ExpensesSummary.jsx
@@ -5,8 +5,13 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
+export const ExpensesSummary = ({
+	expensesCount,
+	expensesTotal,
+	hiddenExpensesCount,
+}) => {
 	const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+	const hiddenExpenseWord = hiddenExpensesCount === 1 ? 'expense' : 'expenses';
 	const formattedExpenseTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
 	return (
@@ -16,6 +21,11 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
 					Viewing <span>{expensesCount}</span> {expenseWord} totalling{' '}
 					<span>{formattedExpenseTotal}</span>
 				</h1>
+				{hiddenExpensesCount !== 0 && (
+					<p className="page-header__subtitle">
+						{hiddenExpensesCount} hidden {hiddenExpenseWord}
+					</p>
+				)}
 				<div className="page-header__action">
 					<Link className="button" to="/create">
 						Add Expense
@@ -31,6 +41,7 @@ const mapStateToProps = (state) => {
 	return {
 		expensesCount: visibleExpenses.length,
 		expensesTotal: selectExpensesTotal(visibleExpenses),
+		hiddenExpensesCount: state.expenses.length - visibleExpenses.length,
 	};
 };
 
--- src/tests/components/ExpensesSummary.test.jsx
+++ src/tests/components/ExpensesSummary.test.jsx
@@ -15,3 +15,24 @@ test('should render ExpenseSummary correctly with multiple expenses', () => {
 	);
 	expect(wrapper).toMatchSnapshot();
 });
+
+test('should not render ExpenseSummary with 0 hidden expense', () => {
+	const wrapper = shallow(
+		<ExpensesSummary hiddenExpensesCount={0} expensesTotal={0} />,
+	);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseSummary correctly with 1 hidden expense', () => {
+	const wrapper = shallow(
+		<ExpensesSummary hiddenExpensesCount={1} expensesTotal={9434} />,
+	);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseSummary correctly with multiple hidden expenses', () => {
+	const wrapper = shallow(
+		<ExpensesSummary hiddenExpensesCount={20} expensesTotal={9434} />,
+	);
+	expect(wrapper).toMatchSnapshot();
+});
